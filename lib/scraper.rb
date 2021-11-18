c7e3c3368fbc8ce75011a4118a740c4dc6e83420
EveOfMaly <john.a.kyeremeh@gmail.com>
2021-11-18 16:16:54 -0500

    10% complete

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,29 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = URI.open(index_url) #returns the html content of the page 
+    doc = Nokogiri::HTML(html)
+
+
+    #page: doc = Nokogiri::HTML(html)
+    # student =  doc.css(".student-card")
+    # name =  student.css(".student-name").text
+    # location = student.css(".student-location").text
+    # profile_url = doc.css(".student-card a").attribute("href").value
+
+    students = {}
+
+    #iterate through the students 
+    doc.css(".student-card").each do |student|
+      students[:name] =  student.css("h4.student-name").children.text,
+      students[:location] =  student.css(".student-location").text,
+      students[:profile_url] = doc.css(".student-card a").attribute("href").value
+    end
+
+    #return the student hash
+    students
+   
+   binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
@@ -12,4 +34,9 @@ class Scraper
   end
 
 end
+
+
+
+
+
 
