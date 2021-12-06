ae184f1757596c437066377cdd5b2aae006b1303
EveOfMaly <john.a.kyeremeh@gmail.com>
2021-12-06 11:11:22 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,34 +6,59 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = URI.open(index_url) #returns the html content of the page 
     doc = Nokogiri::HTML(html)
+    
+    students = []
+
+    #iterate through the students 
+    doc.css(".student-card").each do |student| 
+      student_hash = {}
+
+      student_hash[:name] =  student.css("h4.student-name").text
+      student_hash[:location] =  student.css(".student-location").text
+      student_hash[:profile_url] = student.css("a").attribute("href").value
+      students << student_hash
+    end
+   
+    students
+
+  end
 
 
-    #page: doc = Nokogiri::HTML(html)
-    # student =  doc.css(".student-card")
-    # name =  student.css(".student-name").text
-    # location = student.css(".student-location").text
-    # profile_url = doc.css(".student-card a").attribute("href").value
+  def self.scrape_profile_page(profile_url)
+    html = URI.open(profile_url) #returns the html content of the page 
+    doc = Nokogiri::HTML(html)
 
     students = {}
 
-    #iterate through the students 
-    doc.css(".student-card").each do |student|
-      students[:name] =  student.css("h4.student-name").children.text,
-      students[:location] =  student.css(".student-location").text,
-      students[:profile_url] = doc.css(".student-card a").attribute("href").value
+
+    profile = doc.css(".vitals-container")
+
+    links = profile.css(".social-icon-container").css("a").map { |el| el.attribute('href').value}
+    
+    links.each do |link| 
+      if link.include?("twitter")
+        students[:twitter] = link
+      elsif link.include?("linkedin")
+        students[:linkedin] = link
+      elsif link.include?("github")
+        students[:github] = link
+      elsif  link.include?("blog")
+        students[:blog] = link
+      end 
     end
 
-    #return the student hash
-    students
    
-   binding.pry
-  end
+    profile_quote = profile.css(".profile-quote").text
+    students[:profile_quote] = profile_quote 
+    bio = doc.css(".bio-content").css(".description-holder").text.strip
+    students[:bio] = bio
 
-  def self.scrape_profile_page(profile_url)
-    
-  end
+  
+    students 
 
+  end
 end
+
 
 
 
